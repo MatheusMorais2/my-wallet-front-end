e46d473f8908cecad5422f0ac7edca4c192c0f1a
Matheus <morais.matheus.web@gmail.com>
2022-02-07 13:06:54 -0300

    Display extract in main page

--- src/MainPage/Main/index.js
+++ src/MainPage/Main/index.js
@@ -7,6 +7,7 @@ import UserContext from "../../Contexts/userContext";
 export default function Main() {
   const { userData } = useContext(UserContext);
   const [extract, setExtract] = useState([]);
+  const [balance, setBalance] = useState(0);
 
   useEffect(() => {
     const config = {
@@ -18,18 +19,36 @@ export default function Main() {
 
     promise.then((res) => {
       setExtract(res.data);
+
+      let aux = 0;
+      res.data.forEach((element) => {
+        if (element.type === "deposit") aux += parseFloat(element.value);
+        else aux -= parseFloat(element.value);
+      });
+
+      setBalance(aux);
     });
   }, []);
 
-  console.log("cu", extract);
-
   return (
     <Box>
-      {extract.map((elem) => {
-        <Transaction>
-          Valor: {elem.value}, tipo: {elem.type}
-        </Transaction>;
-      })}
+      <Transactions>
+        {extract.map((elem) => {
+          return (
+            <Transaction key={elem.value + elem.type + elem.date}>
+              <Aux>
+                <Date>{elem.date}</Date>
+                <Description>{elem.description}</Description>
+              </Aux>
+
+              <Value type={elem.type}>{elem.value}</Value>
+            </Transaction>
+          );
+        })}
+      </Transactions>
+      <Balance>
+        <B>SALDO</B> <Total balance={balance}>{balance}</Total>
+      </Balance>
     </Box>
   );
 }
@@ -40,8 +59,54 @@ const Box = styled.div`
   height: 66vh;
   border-radius: 5px;
   margin-bottom: 20px;
+  padding: 23px 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
-const Transaction = styled.p`
-  color: black;
+const Transactions = styled.div``;
+
+const Transaction = styled.div`
+  font-family: "Raleway";
+  font-size: 16px;
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+const Aux = styled.div``;
+
+const Date = styled.span`
+  color: #c6c6c6;
+  margin-right: 5px;
+`;
+
+const Description = styled.span`
+  color: #000;
+`;
+
+const Value = styled.span`
+  color: ${(props) => (props.type === "deposit" ? "#03AC00" : "red")};
+  text-align: right;
+`;
+
+const Balance = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const B = styled.div`
+  font-family: "Raleway";
+  font-size: 17px;
+  font-weight: 700;
+  color: #000;
+`;
+
+const Total = styled.div`
+  font-family: "Raleway";
+  font-size: 17px;
+  font-weight: 400;
+  color: ${(props) => (props.balance >= 0 ? "#03AC00" : "red")};
 `;
